f0162ba4bf4515152872dc471412862f307bbe1e
Eriks Vitolins <evitolins@gmail.com>
2015-11-08 01:45:36 -0800

    Updated example with simple canvas drawing

--- example/example.js
+++ example/example.js
@@ -13,35 +13,50 @@
                 x : (e.pageX - offset.left),
                 y : (e.pageY - offset.top)
             };
-        };
+        },
+
+        clear = function () {
+            var ctx = elem.getContext('2d');
+            ctx.clearRect(0, 0, elem.width, elem.height);
+        },
+
+        draw = function (x, y, fillStyle, size) {
+            var ctx = elem.getContext('2d');
+            size = size || 1;
+            ctx.fillStyle = fillStyle;
+            ctx.fillRect(x - size / 2, y - size / 2, size, size);
+        },
+        isDrawing = false;
 
     cheese.addRoute('route1', {
-        'mouseover' : function (e) { console.log('route1: mouseover', relCoords(e, this).x, relCoords(e, this).y ); },
-        'mouseout'  : function (e) { console.log('route1: mouseout', relCoords(e, this).x, relCoords(e, this).y ); },
-        'mousedown' : function (e) { console.log('route1: mousedown', relCoords(e, this).x, relCoords(e, this).y ); },
-        'mousemove' : function (e) { console.log('route1: mousemove', relCoords(e, this).x, relCoords(e, this).y ); },
-        'mouseup'   : function (e) { console.log('route1: mouseup', relCoords(e, this).x, relCoords(e, this).y ); }
+        'mouseover' : function (e) { console.log('route1: mouseover', relCoords(e, this).x, relCoords(e, this).y); },
+        'mouseout'  : function (e) { console.log('route1: mouseout', relCoords(e, this).x, relCoords(e, this).y); },
+        'mousedown' : function (e) { isDrawing = true; draw(relCoords(e, this).x, relCoords(e, this).y, "red", 1); },
+        'mousemove' : function (e) { if (isDrawing) { draw(relCoords(e, this).x, relCoords(e, this).y, "red", 1); } },
+        'mouseup'   : function (e) { isDrawing = false; }
     });
     cheese.addRoute('route2', {
-        'mousedown' : function (event) { console.log('route2: mousedown', event.clientX, event.clientY); },
-        'mouseup'   : function (event) { console.log('route2: mouseup', event.clientX, event.clientY); }
+        'mousedown' : function (e) { isDrawing = true; draw(relCoords(e, this).x, relCoords(e, this).y, "blue", 4); },
+        'mousemove' : function (e) { if (isDrawing) { draw(relCoords(e, this).x, relCoords(e, this).y, "blue", 4); } },
+        'mouseup'   : function (e) { isDrawing = false; }
     });
     cheese.addRoute('route3', {
-        'mousedown' : function (e) { console.log('route3: mousedown', relCoords(e, this).x, relCoords(e, this).y ); },
-        'mousemove' : function (e) { console.log('route3: mousemove', relCoords(e, this).x, relCoords(e, this).y ); },
-        'mouseup'   : function (e) { console.log('route3: mouseup', relCoords(e, this).x, relCoords(e, this).y ); }
+        'mousedown' : function (e) { isDrawing = true; draw(relCoords(e, this).x, relCoords(e, this).y, "green", 8); },
+        'mousemove' : function (e) { if (isDrawing) draw(relCoords(e, this).x, relCoords(e, this).y, "green", 8); },
+        'mouseup'   : function (e) { isDrawing = false; }
     });
 
     var btn1 = document.getElementById('button1');
     var btn2 = document.getElementById('button2');
     var btn3 = document.getElementById('button3');
-    btn1.addEventListener('click', function () { cheese.setRoute('route1');} );
-    btn2.addEventListener('click', function () { cheese.setRoute('route2');} );
-    btn3.addEventListener('click', function () { cheese.setRoute('route3');} );
+    var btn4 = document.getElementById('button4');
+    btn1.addEventListener('click', function () { cheese.setRoute('route1'); });
+    btn2.addEventListener('click', function () { cheese.setRoute('route2'); });
+    btn3.addEventListener('click', function () { cheese.setRoute('route3'); });
+    btn4.addEventListener('click', function () { clear(); });
 
     cheese.setRoute('route1');
 
     window.cheese = cheese;
 
 }(Cheese, document, console));
-
